e0952fcadaf32d83c639f95531fca10cfd987b78
Takaki Matsumoto <mtakaking@users.noreply.github.com>
2017-08-07 18:40:44 -0400

    Add files via upload

---
+++ O365_PasswordExpiration.ps1
@@ -0,0 +1,7 @@
+﻿
+$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Connect-MsolService -Credential $UserCredential
+
+$staffToEmail = Get-MsolUser -all | Where-Object {$_.isLicensed -eq $true -and $_.Licenses[0].ServiceStatus[8].ProvisioningStatus -eq "Disabled"} | select DisplayName, userprincipalname, LastPasswordChangeTimeStamp,@{Name=”PasswordAge”;Expression={(Get-Date)-$_.LastPasswordChangeTimeStamp}} | where {$_.PasswordAge -le “89” -and $_.PasswordAge -gt "80"} | sort-object PasswordAge -descending
+$staffToEmail | Export-Csv -Path C:\Users\$env:username\Desktop\O365_PasswordExpiration.csv -NoTypeInformation
